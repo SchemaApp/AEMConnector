3ebb8ae4d2505f581474af671f2488f047deb306
Nikhil Ajudiya <NikhilAjudiya@kemet.com>
2022-01-13 21:31:49 +0530

    junit test cases fixes.

--- core/src/test/java/com/schemaapp/core/services/WebhookHandlerServiceTest.java
+++ core/src/test/java/com/schemaapp/core/services/WebhookHandlerServiceTest.java
@@ -1,5 +1,6 @@
 package com.schemaapp.core.services;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
@@ -32,6 +33,7 @@ import com.day.cq.search.Query;
 import com.day.cq.search.QueryBuilder;
 import com.day.cq.search.result.Hit;
 import com.day.cq.search.result.SearchResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.schemaapp.core.models.WebhookEntity;
 import com.schemaapp.core.services.impl.WebhookHandlerServiceImpl;
 
@@ -39,6 +41,8 @@ import com.schemaapp.core.services.impl.WebhookHandlerServiceImpl;
 class WebhookHandlerServiceTest {
 
 	private static final String GRAPH_DATA = "[{\"@context\":\"http://schema.org\",\"@type\":\"Product\",\"@id\":\"https://dell.ca/products/monitors/dellultra30\",\"aggregateRating\":{\"@type\":\"AggregateRating\",\"bestRating\":\"100\",\"ratingCount\":\"24\",\"ratingValue\":\"87\"},\"image\":\"dell-30in-lcd.jpg\",\"name\":\"Dell UltraSharp 30\\\" LCD Monitor\",\"offers\":{\"@type\":\"AggregateOffer\",\"highPrice\":\"$1495\",\"lowPrice\":\"$1250\"}}]";
+
+	private static final String JSONL_D = "{\"@context\":{\"@vocab\":\"http://hunchmanifest.com/ontology/Application#\",\"generatedAtTime\":\"http://www.w3.org/ns/prov#generatedAtTime\"},\"@type\":\"EntityCreated\",\"@id\":\"http://localhost:4502/page33\",\"base64encode\":\"aHR0cHM6Ly9kZWxsLmNhL3Byb2R1Y3RzL21vbml0b3JzL2RlbGx1bHRyYTMw\",\"url\":\"https://data.schemaapp.com/ACCOUNTID/aHR0cHM6Ly9kZWxsLmNhL3Byb2R1Y3RzL21vbml0b3JzL2RlbGx1bHRyYTMw\",\"generatedAtTime\":\"2018-02-15T10:20:00Z\",\"@graph\":[{\"@context\":\"http://schema.org\",\"@type\":\"Product\",\"@id\":\"https://dell.ca/products/monitors/dellultra30\",\"aggregateRating\":{\"@type\":\"AggregateRating\",\"bestRating\":\"100\",\"ratingCount\":\"24\",\"ratingValue\":\"87\"},\"image\":\"dell-30in-lcd.jpg\",\"name\":\"Dell UltraSharp 30\\\" LCD Monitor\",\"offers\":{\"@type\":\"AggregateOffer\",\"highPrice\":\"$1495\",\"lowPrice\":\"$1250\"}}]}";
 
 	@Mock
 	private ResourceResolver resolver;
@@ -73,6 +77,8 @@ class WebhookHandlerServiceTest {
 	@InjectMocks
 	private final WebhookHandlerServiceImpl webhookHandlerService = new WebhookHandlerServiceImpl();
 
+	private static ObjectMapper MAPPER = new ObjectMapper().configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+	
 	@Test
 	void createEntityTest() throws Exception {
 
@@ -85,14 +91,15 @@ class WebhookHandlerServiceTest {
 		when(node.addNode(anyString(), eq(JcrConstants.NT_UNSTRUCTURED))).thenReturn(node);
 		
 		webhookHandlerService.createEntity(entity);
-		verify(resolver, times(2)).commit();
+		verify(resolver, times(1)).commit();
 	}
 	
 	@Test
 	void updateEntityTest() throws Exception {
 
 		mockResolver();
-		when(entity.getGraph()).thenReturn(new String(GRAPH_DATA));
+		final WebhookEntity mockEntity = MAPPER.readValue(JSONL_D, WebhookEntity.class);
+		when(entity.getGraph()).thenReturn(mockEntity.getGraph());
 		when(entity.getId()).thenReturn(new String("https://dell.ca/products/monitors/dellultra30"));
 		when(resource.adaptTo(Node.class)).thenReturn(node);
 		when(resolver.adaptTo(Session.class)).thenReturn(session); //maybe not needed
