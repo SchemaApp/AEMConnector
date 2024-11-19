092ba8c3ed5c8ec2a2df6e358b62bef98dd33286
Brian Langeland <brian.langeland@skullandraven.tech>
2024-11-19 15:14:20 -0600

    Issue #17502 - added JavaDocs

--- core/src/main/java/com/schemaapp/core/util/UrlUtils.java
+++ core/src/main/java/com/schemaapp/core/util/UrlUtils.java
@@ -6,6 +6,13 @@ public final class UrlUtils {
 
     private UrlUtils() {}
 
+    /**
+     * Concatenates the site URL (the domain) and a path to a page, removing a trailing / if one
+     * exists after the site URL.
+     * @param siteUrl The site URL
+     * @param path The path to the page
+     * @return String of the concatenated siteUrl and path.
+     */
     public static String concatSiteUrlPath(String siteUrl, String path) {
         if (StringUtils.endsWith(siteUrl, "/") && StringUtils.startsWith(path, "/")) {
             return StringUtils.removeEnd(siteUrl, "/") + path;
